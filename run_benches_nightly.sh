d2dcfaeeb0606f9dc6bd0bb3e544a0435781dac1
brycx <brycx@protonmail.com>
2019-02-13 16:17:13 +0100

    mkdir bench-results

--- run_benches_nightly.sh
+++ run_benches_nightly.sh
@@ -1,5 +1,6 @@
 for file in ct-bencher/*[.rs];
     do  
+        mkdir bench-results;
         filename=$(basename "$file");
         fname="${filename%.*}"; # Filename without .rs extension
         cargo +nightly run --no-default-features --features nightly --bin $fname > bench-results/$fname.txt;
--- run_benches_stable.sh
+++ run_benches_stable.sh
@@ -1,5 +1,6 @@
 for file in ct-bencher/*[.rs];
     do  
+        mkdir bench-results;
         filename=$(basename "$file");
         fname="${filename%.*}"; # Filename without .rs extension
         cargo run --bin $fname > bench-results/$fname.txt;
