1eb0b3be4fb1e24799791d3fe8e29d8bf2b21da0
brycx <brycx@protonmail.com>
2019-10-06 13:19:27 +0200

    CI: Clean up CI scripts

--- run_benches_nightly_release.sh
+++
@@ -1,7 +0,0 @@
-for file in ct-bencher/*[.rs];
-    do  
-        mkdir bench-results;
-        filename=$(basename "$file");
-        fname="${filename%.*}"; # Filename without .rs extension
-        RUSTFLAGS="-C $1" cargo +nightly run --release --no-default-features --features nightly --bin $fname > bench-results/$fname.txt;
-    done
