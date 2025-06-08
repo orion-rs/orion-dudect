8dc117b9943c7c8d09637eac0cf4f7a83a4f76d2
brycx <brycx@protonmail.com>
2025-06-08 17:11:38 +0200

    Run stable only (save CI time and the most important and non-regressing channel)

--- run_benches_release.sh
+++ run_benches_release.sh
@@ -3,9 +3,3 @@ cargo clean;
 mkdir bench-results;
 RUSTFLAGS="-C $1" cargo run --release --bin ct_benches > bench-results/ct_benches.txt;
 cargo test -- --nocapture;
-
-# nightly
-cargo clean;
-mkdir bench-results;
-RUSTFLAGS="-C $1" cargo +nightly run --release --bin ct_benches > bench-results/ct_benches.txt;
-cargo test -- --nocapture;
