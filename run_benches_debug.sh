8e3310688e592336a4a2c5debd8226f9ca709f02
brycx <brycx@protonmail.com>
2025-06-08 15:39:34 +0200

    Update CI workflows, copyright year and debug-bash script

--- run_benches_debug.sh
+++ run_benches_debug.sh
@@ -1,11 +1,11 @@
 # stable
 cargo clean;
 mkdir bench-results;
-RUSTFLAGS="-C $1" cargo run --bin ct_benches --target $2 > bench-results/ct_benches.txt;
+RUSTFLAGS="-C $1" cargo run --bin ct_benches > bench-results/ct_benches.txt;
 cargo test -- --nocapture;
 
 # nightly
 cargo clean;
 mkdir bench-results;
-RUSTFLAGS="-C $1" cargo +nightly run --bin ct_benches --target $2 > bench-results/ct_benches.txt;
+RUSTFLAGS="-C $1" cargo +nightly run --bin ct_benches > bench-results/ct_benches.txt;
 cargo test -- --nocapture;
