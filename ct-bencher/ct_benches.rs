4676f9f10695699b038b520364287c457a059e98
brycx <brycx@protonmail.com>
2025-06-08 16:33:42 +0200

    Add MLKEM-relatede ct-benches

--- ct-bencher/ct_benches.rs
+++ ct-bencher/ct_benches.rs
@@ -135,6 +135,133 @@ fn test_x25519_scalarmul(runner: &mut CtRunner, rng: &mut BenchRng) {
     }
 }
 
+// src: https://github.com/orion-rs/orion/blob/master/src/hazardous/kem/ml_kem/internal/fe.rs
+const KYBER_Q: u32 = 3329;
+
+fn conditional_sub_u32(a: u32) -> u32 {
+    // Calculate a - mod
+    let t: u32 = a.overflowing_sub(KYBER_Q).0;
+
+    // Check if a >= mod (if t is non-negative)
+    // If a >= mod, mask will be 0xFFFFFFF, otherwise 0
+    let mask: u32 = 0u32.overflowing_sub(t >> 31).0;
+
+    // If mask is 0, return a (no subtraction), otherwise return t (a - mod)
+    (t & !mask) | (a & mask)
+}
+
+fn barrett_reduce(value: u32) -> u32 {
+    debug_assert!(value < KYBER_Q.pow(2));
+
+    const MUL: u64 = 5039;
+    const SHIFT: u64 = 24;
+
+    let quo: u32 = ((u64::from(value) * MUL) >> SHIFT) as u32;
+    let r = value - (quo * KYBER_Q);
+    // NOTE: Guaranteed now 0 <= r < 2q. This is where we add the
+    // conditional subtraction.
+    debug_assert!((0..KYBER_Q * 2).contains(&r));
+
+    let ret = conditional_sub_u32(r);
+    debug_assert!((0..KYBER_Q).contains(&ret));
+
+    ret
+}
+
+fn test_mlkem_barrett_reduce(runner: &mut CtRunner, rng: &mut BenchRng) {
+    let mut inputs: Vec<u32> = Vec::new();
+    let mut classes = Vec::new();
+
+    for _ in 0..NUMBER_OF_SAMPLES {
+        // "Given value < 2q return value mod q (in [0, n])."
+        inputs.push(rng.gen_range(0..(KYBER_Q * KYBER_Q)));
+
+        if rng.gen::<bool>() {
+            classes.push(Class::Left);
+        } else {
+            classes.push(Class::Right);
+        }
+    }
+
+    for (class, k) in classes.into_iter().zip(inputs.into_iter()) {
+        runner.run_one(class, || barrett_reduce(k));
+    }
+}
+
+// src: https://github.com/orion-rs/orion/blob/master/src/hazardous/kem/ml_kem/internal/fe.rs
+fn decompress(y: u32, d: u8) -> u32 {
+    debug_assert!((1..=11).contains(&d));
+
+    let div: u32 = y * KYBER_Q;
+    let mut quo: u32 = div >> d as u32;
+    quo += (div >> (d as u32 - 1)) & 1;
+
+    debug_assert!(quo < KYBER_Q);
+
+    quo
+}
+
+fn compress(fe: u32, d: u8) -> u32 {
+    debug_assert!((1..=11).contains(&d));
+
+    const MUL: u64 = 5039;
+    const SHIFT: u64 = 24;
+
+    let div: u32 = fe << d;
+    let mut quo: u32 = ((u64::from(div) * MUL) >> SHIFT) as u32;
+    let rem: u32 = div - (quo * KYBER_Q);
+
+    quo += ((KYBER_Q / 2).overflowing_sub(rem).0 >> 31) & 1;
+    quo += ((KYBER_Q + KYBER_Q / 2 - rem) >> 31) & 1;
+
+    let mask: u32 = (1 << d as u32) - 1;
+
+    ((quo & mask) as u16) as u32
+}
+
+fn test_compress<const D: u8>(runner: &mut CtRunner, rng: &mut BenchRng) {
+    let mut inputs: Vec<u32> = Vec::new();
+    let mut classes = Vec::new();
+
+    for _ in 0..NUMBER_OF_SAMPLES {
+        inputs.push(rng.gen_range(0..2u32.pow(D as u32)));
+
+        if rng.gen::<bool>() {
+            classes.push(Class::Left);
+        } else {
+            classes.push(Class::Right);
+        }
+    }
+
+    for (class, x) in classes.into_iter().zip(inputs.into_iter()) {
+        runner.run_one(class, || compress(decompress(x, D), D));
+    }
+}
+
+fn test_compress_d1(runner: &mut CtRunner, rng: &mut BenchRng) {
+    test_compress::<1u8>(runner, rng);
+}
+
+fn test_compress_d4(runner: &mut CtRunner, rng: &mut BenchRng) {
+    test_compress::<4u8>(runner, rng);
+}
+
+fn test_compress_d5(runner: &mut CtRunner, rng: &mut BenchRng) {
+    test_compress::<5u8>(runner, rng);
+}
+
+fn test_compress_d6(runner: &mut CtRunner, rng: &mut BenchRng) {
+    test_compress::<6u8>(runner, rng);
+}
+
+fn test_compress_d10(runner: &mut CtRunner, rng: &mut BenchRng) {
+    test_compress::<10u8>(runner, rng);
+}
+
+fn test_compress_d11(runner: &mut CtRunner, rng: &mut BenchRng) {
+    test_compress::<11u8>(runner, rng);
+}
+
 ctbench_main!(
     test_newtype,
     test_newtype_slice,
@@ -144,5 +271,12 @@ ctbench_main!(
     test_ct_base64_encode,
     test_ct_base64_decode,
     test_x25519_scalarmul_base,
-    test_x25519_scalarmul
+    test_x25519_scalarmul,
+    test_mlkem_barrett_reduce,
+    test_compress_d1,
+    test_compress_d4,
+    test_compress_d5,
+    test_compress_d6,
+    test_compress_d10,
+    test_compress_d11
 );
