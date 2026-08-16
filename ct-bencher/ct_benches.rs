0a079f5410b9fb0635d89e3a848485d2595ea2bb
brycx <brycx@protonmail.com>
2026-08-16 17:07:08 +0200

    Use ML-KEM re-exports

--- ct-bencher/ct_benches.rs
+++ ct-bencher/ct_benches.rs
@@ -23,6 +23,7 @@ use orion::hazardous::dsa::{
     FieldElement, MlDsa44, MlDsa65, MlDsa87, MlDsaParameters, Standard, mldsa44, mldsa65, mldsa87,
     montgomery_reduce,
 };
+use orion::hazardous::kem::{FieldElement as MlKemFieldElement, barrett_reduce};
 
 // We only test one newtype that implements PartialEq, because they
 // all use the macro to implement it.
@@ -141,38 +142,7 @@ fn test_x25519_scalarmul(runner: &mut CtRunner, rng: &mut BenchRng) {
     }
 }
 
-// src: https://github.com/orion-rs/orion/blob/master/src/hazardous/kem/ml_kem/internal/fe.rs
 const KYBER_Q: u32 = 3329;
-
-fn conditional_sub_u32(a: u32) -> u32 {
-    // Calculate a - mod
-    let t: u32 = a.overflowing_sub(KYBER_Q).0;
-
-    // Check if a >= mod (if t is non-negative)
-    // If a >= mod, mask will be 0xFFFFFFF, otherwise 0
-    let mask: u32 = 0u32.overflowing_sub(t >> 31).0;
-
-    // If mask is 0, return a (no subtraction), otherwise return t (a - mod)
-    (t & !mask) | (a & mask)
-}
-
-fn barrett_reduce(value: u32) -> u32 {
-    debug_assert!(value < KYBER_Q.pow(2));
-
-    const MUL: u64 = 5039;
-    const SHIFT: u64 = 24;
-
-    let quo: u32 = ((u64::from(value) * MUL) >> SHIFT) as u32;
-    let r = value - (quo * KYBER_Q);
-    // NOTE: Guaranteed now 0 <= r < 2q. This is where we add the
-    // conditional subtraction.
-    debug_assert!((0..KYBER_Q * 2).contains(&r));
-
-    let ret = conditional_sub_u32(r);
-    debug_assert!((0..KYBER_Q).contains(&ret));
-
-    ret
-}
 
 fn test_mlkem_barrett_reduce(runner: &mut CtRunner, rng: &mut BenchRng) {
     let mut inputs: Vec<u32> = Vec::new();
@@ -194,53 +164,24 @@ fn test_mlkem_barrett_reduce(runner: &mut CtRunner, rng: &mut BenchRng) {
     }
 }
 
-// src: https://github.com/orion-rs/orion/blob/master/src/hazardous/kem/ml_kem/internal/fe.rs
-fn decompress(y: u32, d: u8) -> u32 {
-    debug_assert!((1..=11).contains(&d));
-
-    let div: u32 = y * KYBER_Q;
-    let mut quo: u32 = div >> d as u32;
-    quo += (div >> (d as u32 - 1)) & 1;
-
-    debug_assert!(quo < KYBER_Q);
-
-    quo
-}
-
-fn compress(fe: u32, d: u8) -> u32 {
-    debug_assert!((1..=11).contains(&d));
-
-    const MUL: u64 = 5039;
-    const SHIFT: u64 = 24;
-
-    let div: u32 = fe << d;
-    let mut quo: u32 = ((u64::from(div) * MUL) >> SHIFT) as u32;
-    let rem: u32 = div - (quo * KYBER_Q);
-
-    quo += ((KYBER_Q / 2).overflowing_sub(rem).0 >> 31) & 1;
-    quo += ((KYBER_Q + KYBER_Q / 2 - rem) >> 31) & 1;
-
-    let mask: u32 = (1 << d as u32) - 1;
-
-    ((quo & mask) as u16) as u32
-}
-
 fn test_compress<const D: u8>(runner: &mut CtRunner, rng: &mut BenchRng) {
-    let mut inputs: Vec<u32> = Vec::new();
+    let mut inputs: Vec<MlKemFieldElement> = Vec::new();
     let mut classes = Vec::new();
 
     for _ in 0..NUMBER_OF_SAMPLES {
         if rng.random::<bool>() {
-            inputs.push(KYBER_Q - 1);
+            inputs.push(MlKemFieldElement::new(KYBER_Q - 1));
             classes.push(Class::Left);
         } else {
-            inputs.push(rng.random_range(0..2u32.pow(D as u32)));
+            inputs.push(MlKemFieldElement::new(
+                rng.random_range(0..2u32.pow(D as u32)),
+            ));
             classes.push(Class::Right);
         }
     }
 
     for (class, x) in classes.into_iter().zip(inputs.iter()) {
-        runner.run_one(class, || compress(decompress(*x, D), D));
+        runner.run_one(class, || MlKemFieldElement::decompress(x.compress(D), D));
     }
 }
 
